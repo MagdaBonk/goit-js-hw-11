2125ac6ff58ecab24342b7b43cb8151ba2c360f2
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2022-12-15 19:44:49 +0100

    added required libraries

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
