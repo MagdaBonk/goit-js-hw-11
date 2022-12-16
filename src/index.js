f75b394b4bdfd43046be0108c6e16bb53a5daf19
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2022-12-16 23:30:50 +0100

    working gallery

--- src/index.js
+++ src/index.js
@@ -9,14 +9,36 @@ const DEBOUNCE_DELAY = 300;
 
 const searchForm = document.querySelector('form#search-form');
 const inputSearch = document.querySelector("input[name='searchQuery']");
-const gallery = document.querySelector('div.gallery');
+const gallery = document.querySelector('.gallery');
 
 searchForm.addEventListener('input', _debounce(inputListener, DEBOUNCE_DELAY));
 
-function inputListener(event) {
+inputSearch.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+  }
+});
+
+async function inputListener(event) {
   const searchQuery = event.target.value;
   var URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const imageList = getImages(URL);
+  const imageList = await getImages(URL);
+
+  const createGallery = imageList.hits
+    .map(
+      ({ previewURL, largeImageURL, tags }) =>
+        `<a class="gallery__item" href=${largeImageURL}>
+        <img class="gallery__image" src=${previewURL} alt=${tags}>
+      </a>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+  const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 }
 
 var API_KEY = '32105928-babf9526dde61d2d51f562299';
@@ -24,25 +46,8 @@ var API_KEY = '32105928-babf9526dde61d2d51f562299';
 async function getImages(URL) {
   try {
     const response = await axios.get(URL);
-    console.log(response);
-    return response;
+    return response.data;
   } catch (error) {
     console.error(error);
   }
 }
-
-// const createGallery = imageList.hits
-//   .map(
-//     ({ previewURL, pageURL, tags }) =>
-//       `<a class="gallery__item" href=${pageURL}>
-//       <img class="gallery__image" src=${previewURL} alt=${tags}>
-//     </a>`
-//   )
-//   .join('');
-
-// gallery.insertAdjacentHTML('afterbegin', createGallery);
-
-// const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
