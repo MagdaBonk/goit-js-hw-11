32d115273a180007c3d6dadebde3c586d939122d
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2022-12-17 00:17:26 +0100

    added image details

--- src/index.js
+++ src/index.js
@@ -25,10 +25,37 @@ async function inputListener(event) {
 
   const createGallery = imageList.hits
     .map(
-      ({ previewURL, largeImageURL, tags }) =>
-        `<a class="gallery__item" href=${largeImageURL}>
-        <img class="gallery__image" src=${previewURL} alt=${tags}>
-      </a>`
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `</a>
+      <div class="photo-card gallery__item">
+        <figure>
+        <a class="gallery__item" href=${largeImageURL}>
+      <img class="gallery__image" src=${webformatURL} alt=${tags}" loading="lazy" />
+      </a>
+      <figcaption class="info">
+      <p class="info-item">
+      <b>Likes</b> ${likes}
+      </p>
+      <p class="info-item">
+      <b>Views</b> ${views}
+      </p>
+      <p class="info-item">
+      <b>Comments</b> ${comments}
+      </p>
+      <p class="info-item">
+      <b>Downloads</b> ${downloads}
+      </p>
+      </figcaption>
+      </figure>
+      </div>`
     )
     .join('');
 
